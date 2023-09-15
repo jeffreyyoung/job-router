e73a7de84e2f97970693d3701aa4d29f488e2cd7
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2023-09-14 18:29:03 -0700

    oops

--- src/JobScheduler.ts
+++ src/JobScheduler.ts
@@ -19,7 +19,7 @@ export type JobScheduler<Events extends IEventSchemas> = {
   send: <EventName extends keyof Events>(
     eventName: EventName,
     data: Events[EventName],
-    ops: { delaySeconds?: number }
+    ops?: { delaySeconds?: number }
   ) => Promise<any>;
   sendMany: (jobs: IEventExecutionState<Events, any>[]) => Promise<any>;
 };
